b98b3cf475bb022b4bd12eb657825e8690376fdd
Ghuy13 <dohoanggiahuy130604@gmail.com>
2025-06-17 17:49:39 +0700

    Sort và filter cho product

--- server/src/services/ProductService.js
+++ server/src/services/ProductService.js
@@ -102,24 +102,89 @@ const getDetailsProduct = (id) => {
     })
 }
 
-const getAllProduct = (limit = 8, page = 0) => {
+// const getAllProduct = (limit, page, sort) => {
+//     return new Promise(async (resolve, reject) => {
+//         try {
+//             const totalProduct = await Product.countDocuments()
+//             // console.log("🚀 ~ getAllProduct ~ filter:", filter)
+//             if (sort) {
+//                 const objectSort = {}
+//                 objectSort[sort[1]] = sort[0]
+//                 const allProductSort = await Product.find().limit(limit).skip(page * limit).sort(objectSort)
+//                 resolve({
+//                     status: "OK",
+//                     message: "Success",
+//                     data: allProductSort,
+//                     total: totalProduct,
+//                     pageCurrent: Number(page + 1),
+//                     totalPage: Math.ceil(totalProduct / limit)
+//                 })
+//             }
+//             const allProduct = await Product.find().limit(limit).skip(page * limit).sort({
+//                 name: sort
+//             })
+//             resolve({
+//                 status: "OK",
+//                 message: "Success",
+//                 data: allProduct,
+//                 total: totalProduct,
+//                 pageCurrent: Number(page + 1),
+//                 totalPage: Math.ceil(totalProduct / limit)
+//             })
+//         } catch (error) {
+//             reject(error)
+//         }
+//     })
+// }
+
+const getAllProduct = (limit, page, sort, filter) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const totalProduct = await Product.countDocuments()
-            const allProduct = await Product.find().limit(limit).skip(page * limit)
-            resolve({
-                status: "OK",
-                message: "Success",
-                data: allProduct,
-                total: totalProduct,
-                pageCurrent: Number(page + 1),
-                totalPage: Math.ceil(totalProduct / limit)
-            })
+            const totalProduct = await Product.countDocuments();
+            console.log("filter", filter)
+            if (filter) {
+                const label = filter[0];
+                console.log('label', label);
+                const allObjectFilter = await Product.find({
+                    [label]: { '$regex': filter[1], '$options': 'i' }
+                }).limit(limit).skip(page * limit)
+                resolve({
+                    status: 'OK',
+                    message: 'Success',
+                    data: allObjectFilter,
+                    total: totalProduct,
+                    pageCurrent: Number(page + 1),
+                    totalPage: Math.ceil(totalProduct / limit)
+                });
+            }
+            if (sort) {
+                const objectSort = {};
+                objectSort[sort[1]] = sort[0] === 'asc' ? 1 : -1;
+                const allProductSort = await Product.find().limit(limit).skip(page * limit).sort(objectSort);
+                resolve({
+                    status: "OK",
+                    message: "Success",
+                    data: allProductSort,
+                    total: totalProduct,
+                    pageCurrent: Number(page + 1),
+                    totalPage: Math.ceil(totalProduct / limit)
+                });
+            } else {
+                const allProduct = await Product.find().limit(limit).skip(page * limit);
+                resolve({
+                    status: "OK",
+                    message: "Success",
+                    data: allProduct,
+                    total: totalProduct,
+                    pageCurrent: Number(page + 1),
+                    totalPage: Math.ceil(totalProduct / limit)
+                });
+            }
         } catch (error) {
-            reject(error)
+            reject(error);
         }
-    })
-}
+    });
+};
 
 module.exports = {
     createProduct,
